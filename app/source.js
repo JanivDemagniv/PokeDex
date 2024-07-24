8bab235d4719533408224c589ee27cc0cf52f6db
JanivDemagniv <janivjry@gmail.com>
2024-07-24 15:17:01 +0300

    Add files via upload

--- app/source.js
+++ app/source.js
@@ -1,8 +1,8 @@
 const pokemonArry = [];
 
-async function fillPokemonArray(arr) {
+async function fillPokemonArray(arr, num1, num2) {
     let newArr = []
-    for (let i = 1; i <= 1025; i++) {
+    for (let i = num1; i <= num2; i++) {
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
         let pokemonData = await response.json();
         newArr.push(pokemonData);
@@ -10,9 +10,25 @@ async function fillPokemonArray(arr) {
     return arr = JSON.parse(JSON.stringify(newArr))
 };
 
-let pokemonArr = await fillPokemonArray(pokemonArry);
+let fullPokemonArr151 = [...await fillPokemonArray(pokemonArry, 1, 151)];
+let fullPokemonArr251 = [...await fillPokemonArray(pokemonArry, 152, 251)];
+let fullPokemonArr386 = [...await fillPokemonArray(pokemonArry, 252, 386)];
+let fullPokemonArr493 = [...await fillPokemonArray(pokemonArry, 387, 493)];
+let fullPokemonArr649 = [...await fillPokemonArray(pokemonArry, 494, 649)];
+let fullPokemonArr721 = [...await fillPokemonArray(pokemonArry, 650, 721)];
+let fullPokemonArr809 = [...await fillPokemonArray(pokemonArry, 722, 809)];
+let fullPokemonArr905 = [...await fillPokemonArray(pokemonArry, 810, 905)];
+let fullPokemonArr1025 = [...await fillPokemonArray(pokemonArry, 906, 1025)];
 
-let fullPokemonArr = [...pokemonArr]
+// let fullPokemonArr = [...fullPokemonArr151, ...fullPokemonArr251, ...fullPokemonArr386, ...fullPokemonArr493, ...fullPokemonArr649]
+// async function start() {
+//     let pokemonArr = [];
+//     pokemonArr = await fillPokemonArray(pokemonArry, 1, 151);
+//     return pokemonArr
+// }
+// fullPokemonArr = [...await start()];
+
+
 
 const searchPokemonName = (text) => {
     fullPokemonArr = fullPokemonArr.filter((pokemon) => {
@@ -34,5 +50,5 @@ const reset = () => {
     fullPokemonArr = [...pokemonArr];
 }
 
-export { fullPokemonArr, reset, searchPokemonName, searchPokemonNum }
+export { fullPokemonArr151, fullPokemonArr251, fullPokemonArr386, fullPokemonArr493, fullPokemonArr649, fullPokemonArr721, fullPokemonArr809, fullPokemonArr905, fullPokemonArr1025, reset, searchPokemonName, searchPokemonNum }
 
